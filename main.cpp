fb8284f8104c967eeca866d3022cc32b999d124e
Arslaaan <aochirov@gmail.com>
2021-10-06 20:08:17 +0300

    Тренировочное задание по программированию: Список студентов

--- main.cpp
+++ main.cpp
@@ -5,154 +5,71 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
-// если имя неизвестно, возвращает пустую строку
-string FindNameByYear(const map<int, string> &names, int year) {
-    string name;  // изначально имя неизвестно
-
-    // перебираем всю историю по возрастанию ключа словаря, то есть в хронологическом порядке
-    for (const auto &item : names) {
-        // если очередной год не больше данного, обновляем имя
-        if (item.first <= year) {
-            name = item.second;
-        } else {
-            // иначе пора остановиться, так как эта запись и все последующие относятся к будущему
-            break;
-        }
-    }
-
-    return name;
-}
-
-vector<string> FindPrevNamesByYear(const map<int, string> &names, int year) {
-    vector<string> prevNames;
-    string prevPrevName;
-    // перебираем всю историю по возрастанию ключа словаря, то есть в хронологическом порядке
-    for (const auto &item : names) {
-        // если очередной год не больше данного, обновляем имя
-        if (item.first <= year) {
-            if (prevPrevName != item.second) {
-                prevNames.push_back(item.second);
-            }
-            prevPrevName = item.second;
-        } else {
-            // иначе пора остановиться, так как эта запись и все последующие относятся к будущему
-            break;
-        }
-    }
-
-    reverse(begin(prevNames), end(prevNames));
-    return prevNames;
-}
-
-string adaptHistory(const vector<string> &names) {
-    string historyOfNames = " (";
-    if (names.size() > 1) {
-        for (int i = 1; i < names.size(); ++i) {
-            historyOfNames += names[i] + ", ";
-        }
-        historyOfNames.erase(historyOfNames.size() - 2, historyOfNames.size() - 1);
-        historyOfNames += ")";
-        return historyOfNames;
-    }
-    return "";
-}
-
-class Person {
-public:
-    Person(const string &s1, const string &s2, int y) {
-        first_names[y] = s1;
-        last_names[y] = s2;
-        birthday = y;
-    }
-
-    void ChangeFirstName(int year, const string &first_name) {
-        if (year >= birthday) {
-            first_names[year] = first_name;
-        }
-    }
-
-    void ChangeLastName(int year, const string &last_name) {
-        if (year >= birthday) {
-            last_names[year] = last_name;
-        }
-    }
-
-    string GetFullName(int year) const {
-        if (year < birthday) {
-            return "No person";
-        }
-        // получаем имя и фамилию по состоянию на год year
-        const string first_name = FindNameByYear(first_names, year);
-        const string last_name = FindNameByYear(last_names, year);
-
-        // если и имя, и фамилия неизвестны
-        if (first_name.empty() && last_name.empty()) {
-            return "Incognito";
-
-            // если неизвестно только имя
-        } else if (first_name.empty()) {
-            return last_name + " with unknown first name";
-
-            // если неизвестна только фамилия
-        } else if (last_name.empty()) {
-            return first_name + " with unknown last name";
-
-            // если известны и имя, и фамилия
-        } else {
-            return first_name + " " + last_name;
-        }
-    }
-
-    string GetFullNameWithHistory(int year) const {
-        if (year < birthday) {
-            return "No person";
-        }
-        // получить все имена и фамилии по состоянию на конец года year
-        const string first_name = FindNameByYear(first_names, year);
-        const string last_name = FindNameByYear(last_names, year);
-
-        vector<string> prevNames = FindPrevNamesByYear(first_names, year);
-        vector<string> prevLastNames = FindPrevNamesByYear(last_names, year);
-
-        // если и имя, и фамилия неизвестны
-        if (first_name.empty() && last_name.empty()) {
-            return "Incognito";
-
-            // если неизвестно только имя
-        } else if (first_name.empty()) {
-            return last_name + adaptHistory(prevLastNames) + " with unknown first name";
-
-            // если неизвестна только фамилия
-        } else if (last_name.empty()) {
-            return first_name + adaptHistory(prevNames) + " with unknown last name";
-
-            // если известны и имя, и фамилия
-        } else {
-            return first_name + adaptHistory(prevNames) + " " +
-                   last_name + adaptHistory(prevLastNames);
-        }
-    }
-
-private:
-    map<int, string> first_names;
-    map<int, string> last_names;
-    int birthday;
+struct Student {
+    string name;
+    string surname;
+    int day;
+    int month;
+    int year;
 };
 
+void PrintError() {
+    cout << "bad request" << endl;
+}
+
+bool CorrectName(const string &name) {
+    return name.size() >= 1 && name.size() <= 15;
+}
+
+bool CorrectDate(const int &date) {
+    return date >= 0 && date <= 1000000000;
+}
+
 int main() {
-    Person person("Polina", "Sergeeva", 1960);
-    for (int year : {1959, 1960}) {
-        cout << person.GetFullNameWithHistory(year) << endl;
+    vector<Student> students;
+    int n, m;
+    cin >> n;
+    if (n < 0 || n > 10000) {
+        PrintError();
+        return 0;
     }
-
-    person.ChangeFirstName(1965, "Appolinaria");
-    person.ChangeLastName(1967, "Ivanova");
-    for (int year : {1965, 1967}) {
-        cout << person.GetFullNameWithHistory(year) << endl;
+    for (int i = 0; i < n; ++i) {
+        string name, surname;
+        int day, month, year;
+        cin >> name >> surname >> day >> month >> year;
+        if (CorrectName(name) && CorrectName(surname) &&
+            CorrectDate(day) && CorrectDate(month) && CorrectDate(year)) {
+            students.push_back({name, surname, day, month, year});
+        } else {
+            PrintError();
+        }
     }
-
+    cin >> m;
+    if (m < 0 || m > 10000) {
+        PrintError();
+        return 0;
+    }
+    for (int i = 0; i < m; ++i) {
+        string request;
+        int number;
+        cin >> request >> number;
+        if (CorrectDate(number) && CorrectName(request) && number >= 1 && number <= n) {
+            if (request == "name") {
+                cout << students[number - 1].name << " " << students[number - 1].surname << endl;
+            } else if (request == "date") {
+                Student student = students[number - 1];
+                cout << student.day << "." << student.month << "." << student.year << endl;
+            } else {
+                PrintError();
+            }
+        } else {
+            PrintError();
+        }
+    }
     return 0;
 }
