d7056f31463173fe99a32c84919df5c6811431e0
Arslaaan <aochirov@gmail.com>
2021-10-09 21:40:38 +0300

    part 4 rational

--- main.cpp
+++ main.cpp
@@ -55,51 +55,120 @@ public:
     int Numerator() const {
         return p;
     }
+
     int Denominator() const {
         return q;
     }
+
+    friend ostream &operator<<(ostream &os, const Rational &rational);
 };
 
-bool operator==(const Rational& r1, const Rational& r2) {
+bool operator==(const Rational &r1, const Rational &r2) {
     return r1.Numerator() == r2.Numerator() && r1.Denominator() == r2.Denominator();
 }
 
-Rational operator+(const Rational& r1, const Rational& r2) {
-    return Rational(r1.Numerator()*r2.Denominator() + r2.Numerator()*r1.Denominator(), r1.Denominator()*r2.Denominator());
+Rational operator+(const Rational &r1, const Rational &r2) {
+    return Rational(r1.Numerator() * r2.Denominator() + r2.Numerator() * r1.Denominator(),
+                    r1.Denominator() * r2.Denominator());
 }
 
-Rational operator-(const Rational& r1, const Rational& r2) {
-    return Rational(r1.Numerator()*r2.Denominator() - r2.Numerator()*r1.Denominator(), r1.Denominator()*r2.Denominator());
+Rational operator-(const Rational &r1, const Rational &r2) {
+    return Rational(r1.Numerator() * r2.Denominator() - r2.Numerator() * r1.Denominator(),
+                    r1.Denominator() * r2.Denominator());
 }
 
-Rational operator*(const Rational& r1, const Rational& r2) {
-    return Rational(r1.Numerator()*r2.Numerator(), r1.Denominator()*r2.Denominator());
+Rational operator*(const Rational &r1, const Rational &r2) {
+    return Rational(r1.Numerator() * r2.Numerator(), r1.Denominator() * r2.Denominator());
 }
 
-Rational operator/(const Rational& r1, const Rational& r2) {
-    return Rational(r1.Numerator()*r2.Denominator(), r1.Denominator()*r2.Numerator());
+Rational operator/(const Rational &r1, const Rational &r2) {
+    return Rational(r1.Numerator() * r2.Denominator(), r1.Denominator() * r2.Numerator());
+}
+
+ostream &operator<<(ostream &os, const Rational &rational) {
+    os << rational.p << "/" << rational.q;
+    return os;
+}
+
+istream &operator>>(istream &is, Rational &rational) {
+    int p, q = -939393939;
+    if (!is.eof()) {
+        is >> p;
+        if (!is.eof()) {
+            string d;
+            is >> d;
+            if (d[0] == '/' && d.size() > 1) {
+                stringstream ss(d.substr(1, d.size() - 1));
+                ss >> q;
+                rational = Rational(p, q);
+            }
+        }
+    }
+    return is;
 }
 
 int main() {
     {
-        Rational a(2, 3);
-        Rational b(4, 3);
-        Rational c = a * b;
-        bool equal = c == Rational(8, 9);
-        if (!equal) {
-            cout << "2/3 * 4/3 != 8/9" << endl;
+        ostringstream output;
+        output << Rational(-6, 8);
+        if (output.str() != "-3/4") {
+            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
             return 1;
         }
     }
 
     {
-        Rational a(5, 4);
-        Rational b(15, 8);
-        Rational c = a / b;
-        bool equal = c == Rational(2, 3);
+        istringstream input("5/7");
+        Rational r;
+        input >> r;
+        bool equal = r == Rational(5, 7);
         if (!equal) {
-            cout << "5/4 / 15/8 != 2/3" << endl;
+            cout << "5/7 is incorrectly read as " << r << endl;
             return 2;
+        }
+    }
+
+    {
+        istringstream input("");
+        Rational r;
+        bool correct = !(input >> r);
+        if (!correct) {
+            cout << "Read from empty stream works incorrectly" << endl;
+            return 3;
+        }
+    }
+
+    {
+        istringstream input("5/7 10/8");
+        Rational r1, r2;
+        input >> r1 >> r2;
+        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
+            return 4;
+        }
+
+        input >> r1;
+        input >> r2;
+        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
+            return 5;
+        }
+    }
+
+    {
+        istringstream input1("1*2"), input2("1/"), input3("/4");
+        Rational r1, r2, r3;
+        input1 >> r1;
+        input2 >> r2;
+        input3 >> r3;
+        bool correct = r1 == Rational() && r2 == Rational() && r3 == Rational();
+        if (!correct) {
+            cout << "Reading of incorrectly formatted rationals shouldn't change arguments: "
+                 << r1 << " " << r2 << " " << r3 << endl;
+
+            return 6;
         }
     }
 
