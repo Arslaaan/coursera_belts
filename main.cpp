45e94f3fd9d291cb479c6a7814cf06fcab8628b5
Arslaaan <aochirov@gmail.com>
2021-10-09 21:48:42 +0300

    part 5 rational

--- main.cpp
+++ main.cpp
@@ -85,16 +85,21 @@ Rational operator/(const Rational &r1, const Rational &r2) {
     return Rational(r1.Numerator() * r2.Denominator(), r1.Denominator() * r2.Numerator());
 }
 
+bool operator<(const Rational &r1, const Rational &r2) {
+    Rational r = r1 / r2;
+    return r.Numerator() / r.Denominator() < 1.0;
+}
+
 ostream &operator<<(ostream &os, const Rational &rational) {
     os << rational.p << "/" << rational.q;
     return os;
 }
 
 istream &operator>>(istream &is, Rational &rational) {
-    int p, q = -939393939;
-    if (!is.eof()) {
+    int p, q;
+    if (is) {
         is >> p;
-        if (!is.eof()) {
+        if (is) {
             string d;
             is >> d;
             if (d[0] == '/' && d.size() > 1) {
@@ -109,66 +114,32 @@ istream &operator>>(istream &is, Rational &rational) {
 
 int main() {
     {
-        ostringstream output;
-        output << Rational(-6, 8);
-        if (output.str() != "-3/4") {
-            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
+        const set<Rational> rs = {{1, 2}, {1, 25}, {3, 4}, {3, 4}, {1, 2}};
+        if (rs.size() != 3) {
+            cout << "Wrong amount of items in the set" << endl;
             return 1;
         }
-    }
 
-    {
-        istringstream input("5/7");
-        Rational r;
-        input >> r;
-        bool equal = r == Rational(5, 7);
-        if (!equal) {
-            cout << "5/7 is incorrectly read as " << r << endl;
+        vector<Rational> v;
+        for (auto x : rs) {
+            v.push_back(x);
+        }
+        if (v != vector<Rational>{{1, 25}, {1, 2}, {3, 4}}) {
+            cout << "Rationals comparison works incorrectly" << endl;
             return 2;
         }
     }
 
     {
-        istringstream input("");
-        Rational r;
-        bool correct = !(input >> r);
-        if (!correct) {
-            cout << "Read from empty stream works incorrectly" << endl;
+        map<Rational, int> count;
+        ++count[{1, 2}];
+        ++count[{1, 2}];
+
+        ++count[{2, 3}];
+
+        if (count.size() != 2) {
+            cout << "Wrong amount of items in the map" << endl;
             return 3;
-        }
-    }
-
-    {
-        istringstream input("5/7 10/8");
-        Rational r1, r2;
-        input >> r1 >> r2;
-        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
-        if (!correct) {
-            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
-            return 4;
-        }
-
-        input >> r1;
-        input >> r2;
-        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
-        if (!correct) {
-            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
-            return 5;
-        }
-    }
-
-    {
-        istringstream input1("1*2"), input2("1/"), input3("/4");
-        Rational r1, r2, r3;
-        input1 >> r1;
-        input2 >> r2;
-        input3 >> r3;
-        bool correct = r1 == Rational() && r2 == Rational() && r3 == Rational();
-        if (!correct) {
-            cout << "Reading of incorrectly formatted rationals shouldn't change arguments: "
-                 << r1 << " " << r2 << " " << r3 << endl;
-
-            return 6;
         }
     }
 
