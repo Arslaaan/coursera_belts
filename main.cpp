389cdf6d2ea9e019ceb759516ac16b1c7e691779
Arslaaan <aochirov@gmail.com>
2021-10-20 22:26:41 +0300

    simple database lesson

--- main.cpp
+++ main.cpp
@@ -1,122 +1,161 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <iomanip>
 
 using namespace std;
 
-int NOD(int p, int q) {
-    p = abs(p);
-    q = abs(q);
-    while (p != 0 && q != 0) {
-        if (p > q) {
-            p = p % q;
-        } else {
-            q = q % p;
-        }
+struct Date {
+    Date(int day, int month, int year) {
+        this->day = day;
+        this->month = month;
+        this->year = year;
     }
-    return p + q;
+
+    int day;
+    int month;
+    int year;
+};
+
+ostream &operator<<(ostream &out, const Date &date) {
+    out << setw(4) << setfill('0') << date.year << "-";
+    out << setw(2) << setfill('0') << date.month << "-";
+    out << setw(2) << setfill('0') << date.day;
+    return out;
 }
 
-class Rational {
-    int p;
-    int q;
+class History {
+    map<Date, set<string>> events;
+
 public:
-    Rational() {
-        p = 0;
-        q = 1;
+    void add(const Date &date, const string &s) {
+        events[date].insert(s);
     }
 
-    Rational(int numerator, int denominator) {
-        p = numerator;
-        q = denominator;
-        if (q == 0) {
-            throw invalid_argument("invalid argument");
+    void del(const Date &date, const string &s) {
+        if (events.count(date) > 0) {
+            set<string> &current_events = events[date];
+            if (current_events.count(s) > 0) {
+                current_events.erase(s);
+                cout << "Deleted successfully" << endl;
+                return;
+            }
         }
-        if (p == 0) {
-            q = 1;
-        } else if ((double) p / q < 0) {
-            p = -abs(p);
-            q = abs(q);
-        } else {
-            p = abs(p);
-            q = abs(q);
+        cout << "Event not found" << endl;
+    }
+
+    void del(const Date &date) {
+        if (events.count(date) > 0) {
+            set<string> &current_events = events[date];
+            if (!current_events.empty()) {
+                cout << "Deleted " << current_events.size() << " events" << endl;
+                current_events.clear();
+                return;
+            }
         }
-        int nod = NOD(p, q);
-        while (nod != 1) {
-            p /= nod;
-            q /= nod;
-            nod = NOD(p, q);
+        cout << "Deleted 0 events" << endl;
+    }
+
+    void find(const Date &date) {
+        for (const auto &e: events[date]) {
+            cout << e << endl;
         }
     }
 
-    int Numerator() const {
-        return p;
-    }
-
-    int Denominator() const {
-        return q;
+    void print() {
+        for (const auto &event : events) {
+            for (const auto &set_elem: event.second) {
+                cout << event.first << " " << set_elem << endl;
+            }
+        }
     }
 };
 
-bool operator==(const Rational &r1, const Rational &r2) {
-    return r1.Numerator() == r2.Numerator() && r1.Denominator() == r2.Denominator();
-}
-
-Rational operator+(const Rational &r1, const Rational &r2) {
-    return Rational(r1.Numerator() * r2.Denominator() + r2.Numerator() * r1.Denominator(),
-                    r1.Denominator() * r2.Denominator());
-}
-
-Rational operator-(const Rational &r1, const Rational &r2) {
-    return Rational(r1.Numerator() * r2.Denominator() - r2.Numerator() * r1.Denominator(),
-                    r1.Denominator() * r2.Denominator());
-}
-
-Rational operator*(const Rational &r1, const Rational &r2) {
-    return Rational(r1.Numerator() * r2.Numerator(), r1.Denominator() * r2.Denominator());
-}
-
-Rational operator/(const Rational &r1, const Rational &r2) {
-    if (r1.Denominator() * r2.Numerator() == 0) {
-        throw domain_error("domain error");
+void checkDate(const Date &date) {
+    if (date.month >= 1 && date.month <= 12) {
+        if (date.day >= 1 && date.day <= 31) {
+            //nothing
+        } else {
+            cout << "Day value is invalid: " << date.day << endl;
+            throw runtime_error("");
+        }
+    } else {
+        cout << "Month value is invalid: " << date.month << endl;
+        throw runtime_error("");
     }
-    return Rational(r1.Numerator() * r2.Denominator(), r1.Denominator() * r2.Numerator());
 }
 
-bool operator < (const Rational& lhs, const Rational& rhs) {
-    return (lhs - rhs).Numerator() < 0;
-}
+istream &operator>>(istream &in, Date &date) {
+    string full;
+    in >> full;
+    stringstream ss(full);
 
-ostream &operator<<(ostream &os, const Rational &rational) {
-    os << rational.Numerator() << "/" << rational.Denominator();
-    return os;
-}
-
-istream &operator>>(istream &is, Rational &r) {
-    int n, d;
-    char c;
-    is >> n >> c >> d;
-    if (is && c == '/') {
-        r = Rational(n, d);
+    int y, m, d;
+    char def1, def2;
+    ss >> y >> def1 >> m >> def2 >> d;
+    if (ss && def1 == '-' && def2 == '-' && d >= 0 && m >= 0 && y >= 0 && ss.eof()) {
+        date = Date(d, m, y);
+        checkDate(date);
+    } else {
+        cout << "Wrong date format: " << full << endl;
+        throw runtime_error("");
     }
-    return is;
+    return in;
+}
+
+bool operator<(const Date &date1, const Date &date2) {
+    if (date1.year == date2.year) {
+        if (date1.month == date2.month) {
+            return date1.day < date2.day;
+        } else {
+            return date1.month < date2.month;
+        }
+    } else {
+        return date1.year < date2.year;
+    }
+}
+
+bool operator==(const Date &date1, const Date &date2) {
+    return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
 }
 
 int main() {
-    try {
-        Rational r(1, 0);
-        cout << "Doesn't throw in case of zero denominator" << endl;
-        return 1;
-    } catch (invalid_argument&) {
-    }
+    History history;
+    string line;
 
-    try {
-        auto x = Rational(1, 2) / Rational(0, 1);
-        cout << "Doesn't throw in case of division by zero" << endl;
-        return 2;
-    } catch (domain_error&) {
-    }
+    while (getline(cin, line)) {
+        try {
+            string cmd, event;
+            stringstream lineStream = stringstream(line);
+            Date date(0, 0, 0);
+            lineStream >> cmd;
+            if (cmd == "Add") {
+                lineStream >> date >> event;
+                history.add(date, event);
+            } else if (cmd == "Del") {
+                lineStream >> date;
+                if (lineStream && !lineStream.eof()) {
+                    lineStream >> event;
+                    history.del(date, event);
+                } else {
+                    history.del(date);
+                }
+            } else if (cmd == "Find") {
+                lineStream >> date;
+                history.find(date);
+            } else if (cmd == "Print") {
+                history.print();
+            } else if (!cmd.empty()) {
+                cout << "Unknown command: " << cmd << endl;
+            }
 
-    cout << "OK" << endl;
+        }
+        catch (exception &ex) {
+
+        }
+    }
     return 0;
 }
