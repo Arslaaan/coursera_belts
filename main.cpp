9f88b138bf3aa5a709dc84d6412eb30072d6d0d6
Arslaaan <aochirov@gmail.com>
2021-11-01 19:40:00 +0300

    успешное расписание автобусов

--- main.cpp
+++ main.cpp
@@ -1,61 +1,178 @@
+#include <string>
 #include <iostream>
+#include <cassert>
 #include <vector>
 #include <map>
-#include <algorithm>
 
 using namespace std;
 
-template <typename T>
-vector<T> Sqr(vector<T> vc);
-template <typename Key, typename Value>
-map<Key, Value> Sqr(map<Key, Value> m);
-template <typename First, typename Second>
-pair<First, Second> Sqr(pair<First, Second> vc);
+enum class QueryType {
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses
+};
 
-// Объявляем шаблонные функции
-template<typename T>
-T Sqr(T x) {
-    return x * x;
-}
+struct Query {
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+};
 
-template <typename T>
-vector<T> Sqr(vector<T> vc) {
-    vector<T> result;
-    for (size_t i = 0; i < vc.size(); ++i) {
-        result.push_back(Sqr(vc[i]));
+istream& operator >> (istream& is, Query& q) {
+    // Реализуйте эту функцию
+    string operation_code;
+    is >> operation_code;
+    if (operation_code == "NEW_BUS") {
+        q.type = QueryType::NewBus;
+        is >> q.bus;
+
+        int stop_count;
+        is >> stop_count;
+        q.stops.resize(stop_count);
+        for (int i = 0; i < stop_count; ++i) {
+            is >> q.stops[i];
+        }
+    } else if (operation_code == "BUSES_FOR_STOP") {
+        q.type = QueryType::BusesForStop;
+        is >> q.stop;
+    } else if (operation_code == "STOPS_FOR_BUS") {
+        q.type = QueryType::StopsForBus;
+        is >> q.bus;
+    } else if (operation_code == "ALL_BUSES") {
+        q.type = QueryType::AllBuses;
     }
-    return result;
+    return is;
 }
 
-template <typename Key, typename Value>
-map<Key, Value> Sqr(map<Key, Value> m) {
-    map<Key, Value> result;
-    for(const auto& item: m) {
-        result[item.first] = Sqr(item.second);
+struct BusesForStopResponse {
+    // Наполните полями эту структуру
+    vector<string> buses;
+};
+
+ostream& operator << (ostream& os, const BusesForStopResponse& r) {
+    // Реализуйте эту функцию
+    if (r.buses.empty()) {
+        os << "No stop";
+    } else {
+        for (const string& bus : r.buses) {
+            os << bus << " ";
+        }
     }
-    return result;
+    return os;
 }
 
-template <typename First, typename Second>
-pair<First, Second> Sqr(pair<First, Second> vc) {
-    return make_pair(Sqr(vc.first), Sqr(vc.second));
+struct StopsForBusResponse {
+    // Наполните полями эту структуру
+    string bus;
+    vector<string> stops;
+    map<string, vector<string>> busesForStop; // stop -> bus1, bus2 ...
+};
+
+ostream& operator << (ostream& os, const StopsForBusResponse& r) {
+    // Реализуйте эту функцию
+    if (r.bus.empty()) {
+        os << "No bus";
+    } else {
+        for (const string& stop : r.stops) {
+            os << "Stop " << stop << ": ";
+            if (r.busesForStop.at(stop).size() == 1) {
+                os << "no interchange" << endl;
+            } else {
+                for (const string& other_bus : r.busesForStop.at(stop)) {
+                    if (r.bus != other_bus) {
+                        os << other_bus << " ";
+                    }
+                }
+                os << endl;
+            }
+        }
+    }
+    return os;
 }
+
+struct AllBusesResponse {
+    // Наполните полями эту структуру
+    vector<StopsForBusResponse> stopsForBuses;
+};
+
+ostream& operator << (ostream& os, const AllBusesResponse& r) {
+    // Реализуйте эту функцию
+    if (r.stopsForBuses.empty()) {
+        os << "No buses";
+    } else {
+        for (const auto& stops_for_bus : r.stopsForBuses) {
+            os << "Bus " << stops_for_bus.bus << ": ";
+            for (const string& stop : stops_for_bus.stops) {
+                os << stop << " ";
+            }
+            os << endl; // todo тут возможно лишний эндл
+        }
+    }
+    return os;
+}
+
+class BusManager {
+    map<string, vector<string>> stopsForBus;
+    map<string, vector<string>> busesForStop;
+public:
+    void AddBus(const string& bus, const vector<string>& stops) {
+        // Реализуйте этот метод
+        stopsForBus.insert({bus, stops});
+        for (const string& stop: stops) {
+            busesForStop[stop].push_back(bus);
+        }
+    }
+
+    BusesForStopResponse GetBusesForStop(const string& stop) const {
+        // Реализуйте этот метод
+        if (busesForStop.count(stop) == 0) {return {};}
+        return {busesForStop.at(stop)};
+    }
+
+    StopsForBusResponse GetStopsForBus(const string& bus) const {
+        // Реализуйте этот метод
+        if (stopsForBus.count(bus) == 0) {return {};}
+        return {bus, stopsForBus.at(bus), busesForStop};
+    }
+
+    AllBusesResponse GetAllBuses() const {
+        // Реализуйте этот метод
+        vector<StopsForBusResponse> stopsForBusResponse;
+        for(const auto& stopForBus: stopsForBus) {
+            stopsForBusResponse.push_back({stopForBus.first, stopForBus.second});
+        }
+        return {stopsForBusResponse};
+    }
+};
+
+// Не меняя тела функции main, реализуйте функции и классы выше
 
 int main() {
-    // Пример вызова функции
-    vector<int> v = {1, 2, 3};
-    cout << "vector:";
-    for (int x : Sqr(v)) {
-        cout << ' ' << x;
-    }
-    cout << endl;
+    int query_count;
+    Query q;
 
-    map<int, pair<int, int>> map_of_pairs = {
-            {4, {2, 2}},
-            {7, {4, 3}}
-    };
-    cout << "map of pairs:" << endl;
-    for (const auto &x : Sqr(map_of_pairs)) {
-        cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+    cin >> query_count;
+
+    BusManager bm;
+    for (int i = 0; i < query_count; ++i) {
+        cin >> q;
+        switch (q.type) {
+            case QueryType::NewBus:
+                bm.AddBus(q.bus, q.stops);
+                break;
+            case QueryType::BusesForStop:
+                cout << bm.GetBusesForStop(q.stop) << endl;
+                break;
+            case QueryType::StopsForBus:
+                cout << bm.GetStopsForBus(q.bus) << endl;
+                break;
+            case QueryType::AllBuses:
+                cout << bm.GetAllBuses() << endl;
+                break;
+        }
     }
+
+    return 0;
 }
