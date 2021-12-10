243552b0c4d823c4401bac930a30e9ea16acbfb0
Arslaaan <aochirov@gmail.com>
2021-12-10 19:45:33 +0300

    построение арифметического выражения

--- main.cpp
+++ main.cpp
@@ -16,38 +16,30 @@ void print(const vector<T> &v) {
     cout << endl;
 }
 
-template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(
-        RandomIt range_begin, RandomIt range_end,
-        const string& prefix) {
-    auto left = lower_bound(range_begin, range_end, prefix, [prefix](const auto& x, string y) {
-        return x.substr(0, prefix.size()) < y;
-    });
-    auto right = upper_bound(range_begin, range_end, prefix, [prefix](string y, const auto& x) {
-        return y < x.substr(0, prefix.size());
-    });
-    return {left, right};
-}
-
 int main() {
-    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+    int n, q;
+    cin >> n >> q;
 
-    const auto mo_result =
-            FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-    for (auto it = mo_result.first; it != mo_result.second; ++it) {
-        cout << *it << " ";
+    vector<char> operations = {'.'};
+    string result = to_string(n);
+
+    result.reserve(5 * q + 3);
+    operations.reserve(q);
+
+    int left_brace_count = 0;
+    for (int i = 0; i < q; ++i) {
+        char op;
+        int d;
+        cin >> op >> d;
+        char last_op = operations.back();
+        if ((op == '*' || op == '/') && (last_op == '+' || last_op == '-')){
+            left_brace_count++;
+            result += ") " + string(1, op) + " " + to_string(d);
+        } else {
+            result += " " + string(1, op) + " " + to_string(d);
+        }
+        operations.push_back(op);
     }
-    cout << endl;
-
-    const auto mt_result =
-            FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-    cout << (mt_result.first - begin(sorted_strings)) << " " <<
-         (mt_result.second - begin(sorted_strings)) << endl;
-
-    const auto na_result =
-            FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-    cout << (na_result.first - begin(sorted_strings)) << " " <<
-         (na_result.second - begin(sorted_strings)) << endl;
-
+    cout << string(left_brace_count, '(') + result << endl;
     return 0;
 }
