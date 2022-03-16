c102fbd40bc3246dd89afe773cd9055381f5d430
Arslaaan <aochirov@gmail.com>
2022-03-16 18:14:26 +0300

    booking: start

---
+++ booking/booking.cpp
@@ -0,0 +1,25 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+
+
+int main() {
+    // Для ускорения чтения данных отключается синхронизация
+    // cin и cout с stdio,
+    // а также выполняется отвязка cin от cout
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int query_count;
+    cin >> query_count;
+
+    for (int query_id = 0; query_id < query_count; ++query_id) {
+    }
+
+    return 0;
+}
